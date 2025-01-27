ceaf537d48fe15673a4e0235a7940f1e127b331d
yntymakdev <kubanychevyntymak21@gmail.com>
2025-01-27 14:07:52 +0600

    finish costumized project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  experimental: {
+    missingSuspensWithCSRBailout: false,
+  },
+  env: {
+    SERVER_URL: process.env.SERVER_URL,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.yandex.net",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
---
+++ src/shared/provider/TanstackQueryProvider.tsx
@@ -0,0 +1,16 @@
+import { PropsWithChildren, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export function TanstackQueryProvider({ children }: PropsWithChildren<unknown>) {
+  const [client] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  );
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+}
