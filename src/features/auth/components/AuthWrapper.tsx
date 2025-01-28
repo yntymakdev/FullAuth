71902919208811eecb66802c5bb773142caf8767
yntymakdev <kubanychevyntymak21@gmail.com>
2025-01-28 22:36:08 +0600

    auth wrapper changed

---
+++ src/features/auth/components/AuthWrapper.tsx
@@ -0,0 +1,40 @@
+import { Button, Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/shared/components/ui";
+import { Link } from "lucide-react";
+import React, { type PropsWithChildren } from "react";
+interface AuthWrapperProps {
+  heading: string;
+  description?: string;
+  backButtonLabel?: string;
+  backButtonHref?: string;
+  isShowSocial?: boolean;
+}
+export default function ({
+  children,
+  heading,
+  description,
+  backButtonHref,
+  backButtonLabel,
+  isShowSocial = false,
+}: PropsWithChildren<AuthWrapperProps>) {
+  return (
+    <Card className="w-[400px]">
+      <CardHeader className="space-y-2">
+        <CardTitle>{heading}</CardTitle>
+        {description && <CardDescription>{description}</CardDescription>}
+      </CardHeader>
+
+      <CardContent>
+        {isShowSocial && <div>Social</div>}
+        {children}
+      </CardContent>
+
+      <CardFooter>
+        {backButtonLabel && backButtonHref && (
+          <Button variant="link" className="w-full font-normal">
+            <Link href={backButtonHref}>{backButtonLabel}</Link>
+          </Button>
+        )}
+      </CardFooter>
+    </Card>
+  );
+}
