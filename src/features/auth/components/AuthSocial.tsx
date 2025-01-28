e0a9a126e9c54f992f8a383b3a9913627f45c2b5
yntymakdev <kubanychevyntymak21@gmail.com>
2025-01-29 00:33:49 +0600

    as

--- src/features/auth/components/AuthSocial.tsx
+++ src/features/auth/components/AuthSocial.tsx
@@ -5,7 +5,9 @@ export function AuthSocial() {
     <>
       <div>AuthSocial</div>
       <div className="relative mb-2 space-y-4">
-        <div></div>
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t"></span>
+        </div>
       </div>
     </>
   );
