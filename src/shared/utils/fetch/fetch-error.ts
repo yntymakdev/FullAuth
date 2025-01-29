510777d8c1fba36610e367ec3666afd868dcda03
yntymakdev <kubanychevyntymak21@gmail.com>
2025-01-29 22:48:48 +0600

    fetch inter

--- src/shared/utils/fetch/fetch-error.ts
+++ src/shared/utils/fetch/fetch-error.ts
@@ -1,5 +1,10 @@
-export class FetchError extends Error{
+export class FetchError extends Error {
+	public constructor(
+		public statusCode: number,
+		public message: string
+	) {
+		super(message)
 
-
-    
+		Object.setPrototypeOf(this, new.target.prototype)
+	}
 }
