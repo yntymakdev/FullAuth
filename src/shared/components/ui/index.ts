1b4dc6d3113a22ab609f5975e638b0665c3fd39c
yntymakdev <kubanychevyntymak21@gmail.com>
2025-01-29 12:45:09 +0600

    added ui com

--- src/shared/components/ui/index.ts
+++ src/shared/components/ui/index.ts
@@ -2,3 +2,6 @@ export * from "./Button";
 export * from "./DropdownMenu";
 export * from "./ToggleTheme";
 export * from "./Card";
+export * from "./Input";
+export * from "./Label";
+export * from "./Form";
